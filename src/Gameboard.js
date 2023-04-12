68110f02182ce10fa9ee00126e61ed5d9a25cae4
user <jeffdplante@aol.com>
2023-04-12 08:57:34 -0700

    Complete Gameboard factory and tests

--- src/Gameboard.js
+++ src/Gameboard.js
@@ -6,10 +6,42 @@ function Gameboard() {
     return newBoard;
   }
 
-
   const board = createBoard();
 
   function placeShip(ship, coordinates, direction) {
+    let desiredArea = [coordinates];
+    let dir = {
+      up: [-1, 0],
+      down: [1, 0],
+      left: [0, -1],
+      right: [0, 1],
+    }
+
+    if (!Object.keys(dir).includes(direction)) {
+      throw new Error('Invalid direction provided to placeShip');
+    }
+
+    for (i = 0; i < ship.shipLength; i++) {
+      const lastIndex = desiredArea.length - 1;
+      const transform = dir[direction];
+      const nextCoordinate = [
+        desiredArea[lastIndex][0] + transform[0],
+        desiredArea[lastIndex][1] + transform[1],
+      ];
+      desiredArea.push(nextCoordinate)
+    }
+
+    desiredArea.forEach((coordinate) => {
+      if (coordinate[0] < 0 || coordinate[0] > 10 || coordinate[1] < 0 || coordinate[1] > 10) {
+        throw new Error('Placement overflows board');
+      } if (board[coordinate[0]][coordinate[1]] !== null) {
+        throw new Error('Cannot place over another ship');
+      }
+    });
+
+    desiredArea.forEach((coordinate) => {
+      board[coordinate[0]][coordinate[1]] = ship;
+    });
   }
 
   return { placeShip, board };
--- src/Gameboard.test.js
+++ src/Gameboard.test.js
@@ -4,6 +4,16 @@ Gameboard = require('./Gameboard.js')
 let testBoard;
 beforeEach(() => {
   testBoard = Gameboard();
+  testBoard.boardCompare = function (boardCompared) {
+    for (i = 0; i < this.length; i++) {
+      for (j = 0; j < this[0].length; j++) {
+        if (this[i][j] !== boardCompared[i][j]) {
+          return false; 
+        }
+      }
+    }
+    return true;
+  }
 });
 
 it('Exists', () => {
@@ -16,8 +26,25 @@ it('Creates a correctly formatted 2d array', () => {
 });
 
 it('Has a placeShip() function', () => {
-  testBoard.placeShip();
+  testBoard.placeShip(Ship(4), [0,0], 'down');
 });
 
 it('Correctly places a ship on the board', () => {
+
+  const testShip = Ship(4);
+  testBoard.placeShip(testShip, [0,0], 'right');
+  expect(
+    testBoard.boardCompare([
+      [testShip, testShip, testShip, testShip, null, null, null, null, null, null],
+      [null, null, null, null, null, null, null, null, null, null],
+      [null, null, null, null, null, null, null, null, null, null],
+      [null, null, null, null, null, null, null, null, null, null],
+      [null, null, null, null, null, null, null, null, null, null],
+      [null, null, null, null, null, null, null, null, null, null],
+      [null, null, null, null, null, null, null, null, null, null],
+      [null, null, null, null, null, null, null, null, null, null],
+      [null, null, null, null, null, null, null, null, null, null],
+      [null, null, null, null, null, null, null, null, null, null],
+    ]))
+  .toBe(true);
 });
